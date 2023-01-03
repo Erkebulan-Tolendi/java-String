197f8ec07e8629ea0815ad6f90617aa4bf35489e
Erkeulan-Tolendi <tolendierkebulan555@gmail.com>
2023-01-03 23:47:40 +0600

    String

---
+++ cpt8.java
@@ -0,0 +1,13 @@
+public class cpt8{
+	public static void main(String[] args) {
+        String word = "absdefa";
+
+		int t = word.lastIndexOf("a");
+		int t2 = word.lastIndexOf("d");
+		int t3 = word.lastIndexOf("e");
+
+		System.out.println(t);
+		System.out.println(t2);
+		System.out.println(t3);
+	}
+}
